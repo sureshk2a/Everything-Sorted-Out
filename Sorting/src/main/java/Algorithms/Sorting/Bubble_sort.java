d5838cbb8d1c4b47bc4a7b35af05142a3fd6539e
Suresh <sureshkumar_a@outlook.com>
2019-06-14 15:43:49 +0530

    Initial commit for Bubble Sort

---
+++ Sorting/src/main/java/Algorithms/Sorting/Bubble_sort.java
@@ -0,0 +1,15 @@
+package Algorithms.Sorting;
+
+/*
+ * Bubble Sort
+Bubble Sort is the simplest sorting algorithm that works by repeatedly swapping the adjacent elements if they are in wrong order.
+ * 
+ */
+public class Bubble_sort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
