a84b124f3e43e0fdedb554e8bf0e87e2aa03fe0f
Suresh <sureshkumar_a@outlook.com>
2019-06-15 09:57:07 +0530

    Initial implementation bubble

--- Sorting/src/main/java/Algorithms/Sorting/Bubble_sort.java
+++ Sorting/src/main/java/Algorithms/Sorting/Bubble_sort.java
@@ -1,5 +1,9 @@
 package Algorithms.Sorting;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 /*
  * Bubble Sort
 Bubble Sort is the simplest sorting algorithm that works by repeatedly swapping the adjacent elements if they are in wrong order.
@@ -7,9 +11,52 @@ Bubble Sort is the simplest sorting algorithm that works by repeatedly swapping 
  */
 public class Bubble_sort {
 
+	public static List<Integer> ar = new ArrayList<Integer>();
+	
+	public static void sort1(List<Integer> lst) {
+		int tot  = lst.size();
+		
+		
+		for (int i = 0; i < 3; i++) {		//Checks Three Time
+						
+			for(int j = 0;j<tot;j++)
+			{
+								
+				if(lst.get(j)>lst.get(j+1))
+				{
+					lst.set(j, lst.get(j+1));
+				}
+								
+			}
+					
+		}
+		
+		System.out.println(lst);
+		
+								
+	}
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		 /*Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the total number of integers to sort");
+		 
+		 int tot = scan.nextInt();
+		 
+		 for(int i = 0;i<tot;i++) { tot = scan.nextInt(); ar.add(tot); }*/
+		 
+	
+		ar.add(3);
+		ar.add(1);
+		ar.add(78);
+		ar.add(3);   			//For Test
+		ar.add(9);
+		ar.add(0); 
+
+		sort1(ar);
+		
 	}
 
 }
